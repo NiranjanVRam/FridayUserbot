dcd62751a5eac41a364bb24e1a55abefc6541c98
NiranjanVRam <83854040+NiranjanVRam@users.noreply.github.com>
2021-07-21 18:37:21 +0530

    Update basic_helpers.py

--- main_startup/helper_func/basic_helpers.py
+++ main_startup/helper_func/basic_helpers.py
@@ -194,7 +194,7 @@ def cb_wrapper(func):
         users = await get_all_pros()
         if cb.from_user.id not in users:
             await cb.answer(
-                "You Can't Access Me, Only My Master Can. Why Don't You Deploy Your Own Friday? - @FridayOT",
+                "You Can't Access Me, Only My Master Can. Why Don't You Deploy Your Own Assistant?",
                 cache_time=0,
                 show_alert=True,
             )
@@ -226,7 +226,7 @@ def inline_wrapper(func):
                         InlineQueryResultArticle(
                             title="Sorry, Friend You Can't Use Me!",
                             input_message_content=InputTextMessageContent(
-                                "**Hai!** Well, I am Not For You, I Only Work For My Master. Why Don't You Deploy Your Own @FridayOT ?"
+                                "**Hai!** Well, I am Not For You, I Only Work For My Master. Why Don't You Deploy Your Own Bot?"
                             ),
                         )
                     )
